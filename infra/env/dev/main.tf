0e264d776c562324052d632f19b7d2d300d10476
knpillutla <knpillutla@gmail.com>
2025-06-27 11:55:28 -0400

    updates

--- infra/env/dev/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = var.region
-}
-
-module "ecr" {
-  source              = "../../modules/ecr"
-  ecr_repository_name = var.ecr_repository_name
-  region              = var.region
-}
--- infra/modules/ecr/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-ecr_repo_name = "krishna-dev"
-environment   = "dev"
-aws_region    = "us-east-1"
