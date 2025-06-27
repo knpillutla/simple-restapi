51dcdda08327c7ebe503ce8ec243814377de289b
knpillutla <knpillutla@gmail.com>
2025-06-27 11:49:28 -0400

    updates

---
+++ infra/modules/ecr/terraform.tfvars
@@ -0,0 +1,3 @@
+ecr_repo_name = "krishna-dev"
+environment   = "dev"
+aws_region    = "us-east-1"
--- infra/modules/ecr/variables.tf
+++ infra/modules/ecr/variables.tf
@@ -1,7 +1,14 @@
-variable "ecr_repository_name" {
-  type = string
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
 }
 
-variable "region" {
-  type = string
+variable "ecr_repo_name" {
+  type        = string
+  description = "Name of the ECR repository"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name"
 }
